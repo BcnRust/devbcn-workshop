30db62605649178de0e9c821c3b447a82a49a6b5
Roberto Huertas <roberto.huertas@outlook.com>
2023-06-18 22:17:02 +0200

    feat(db): use shared db

--- api/src/main.rs
+++ api/src/main.rs
@@ -9,7 +9,7 @@ use sqlx::Executor;
 
 #[shuttle_runtime::main]
 async fn actix_web(
-    #[shuttle_aws_rds::Postgres()] pool: sqlx::PgPool,
+    #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     // initialize the database if not already initialized
     pool.execute(include_str!("../db/schema.sql"))
